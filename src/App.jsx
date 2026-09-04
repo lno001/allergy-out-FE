9d38138f285820b4dc8192521bd83a04126a1393
CU0-0 <bill97720@naver.com>
2026-09-04 09:32:46 +0900

    feat : 충돌 제거

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import LoginPage from "./pages/auth/LoginPage";
 import SignupPage from "./pages/auth/SignupPage";
 import Layout from "./components/layout/Layout";
 import Preview from "./preview";
-<<<<<<< HEAD
-import AllergyManagePage from "./pages/mypage/AllergyManagePage";
-import BookmarkListPage from "./pages/mypage/BookmarkListPage";
-=======
 import AllergyManagePage from "./pages/mypage/allergy/AllergyManagePage";
->>>>>>> 302ecc7ef56d2369b62ce635b318f1210aea80ac
+import BookmarkListPage from "./pages/mypage/BookmarkListPage";
 import MyPage from "./pages/mypage/MyPage";
 import ProfileEditPage from "./pages/mypage/ProfileEditPage";
 import RecipeCreatePage from "./pages/recipe/RecipeCreatePage";
